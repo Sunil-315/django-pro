1ce16542067af8d0a37d1dfcdd26a7609000ab38
Sunil-315 <135532616+Sunil-315@users.noreply.github.com>
2025-12-16 16:44:24 +0530

    implemented search feature

--- bookstore/books/views.py
+++ bookstore/books/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
 from django.views.generic import ListView, DetailView
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.db.models import Q
 
 from .models import Book
 
@@ -7,8 +9,21 @@ class BookListView(ListView):
     model = Book
     context_object_name = 'book_list'
     template_name = 'books/book_list.html'
+    login_url = 'account_login'
 
 class BookDetailView(DetailView):
     model = Book
     context_object_name = 'book'
     template_name = 'books/book_detail.html'
+    login_url = 'account_login'
+
+class SearchResultsListView(ListView):
+    model = Book
+    context_object_name = "book_list"
+    template_name = "books/search_results.html"
+
+    def get_queryset(self):
+        query = self.request.GET.get('q')
+        return Book.objects.filter(
+            Q(title__icontains=query) | Q(title__icontains=query)
+        )
